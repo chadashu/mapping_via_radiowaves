674cd70b4dc0ec615c7fce40cdaa627f7346060e
Chad <chadyilmaz@maraofinetech.com>
2023-11-14 08:17:25 +0000

    Switch laptop

--- Arduino/esp32_wroom_module/esp32_wroom_module.ino
+++ Arduino/esp32_wroom_module/esp32_wroom_module.ino
@@ -7,26 +7,32 @@
 #define SERVICE_UUID           "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID    "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
+
 BLEServer *pServer = nullptr;
 BLEService *pService = nullptr;
 BLECharacteristic *pCharacteristic = nullptr;
 bool deviceConnected = false;
 esp_bd_addr_t deviceAddress; // This will hold the address of the connected device
 
-class MyServerCallbacks: public BLEServerCallbacks {
-public:
-    static void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t* param) {
-      switch (event) {
-        case ESP_GAP_BLE_READ_RSSI_COMPLETE_EVT:
-          if (param->read_rssi_cmpl.status == ESP_BT_STATUS_SUCCESS) {
-            Serial.printf("RSSI of connected device: %d dBm\n", param->read_rssi_cmpl.rssi);
-          }
-          break;
-        default:
-          break;
-      }
-    }
+// Define the BLE frequency in Hertz (2400 MHz for BLE)
+const double frequency = 2400e6; // 2.4 GHz in Hz
+const double distanceMeters = 1;
 
+// Function to calculate FSPL in dB, given the distance in meters
+double calculateFSPL() {
+  // Convert frequency to GHz for the FSPL formula
+  double frequencyGHz = frequency / 1e9;
+  // FSPL formula
+  double distanceKilometers = distanceMeters / 1000.0; // Convert meters to kilometers
+
+  double fspl = 20 * log10(distanceKilometers) + 20 * log10(frequencyGHz) + 92.45;
+
+  return fspl;
+}
+
+
+
+class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer, esp_ble_gatts_cb_param_t *param) {
       Serial.println("Client connected");
       deviceConnected = true;
@@ -42,6 +48,21 @@ public:
     }
 };
 
+// Register the GAP event handler with a static member function
+static void my_gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t* param) {
+  if (event == ESP_GAP_BLE_READ_RSSI_COMPLETE_EVT) {
+    if (param->read_rssi_cmpl.status == ESP_BT_STATUS_SUCCESS) {
+      int rssi = param->read_rssi_cmpl.rssi;
+      Serial.printf("RSSI of connected device: %d dBm\n", rssi);
+
+      // Calculate FSPL
+      double fspl = calculateFSPL();
+      Serial.printf("Theoretical FSPL: %f dB\n", fspl);
+
+    }
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   BLEDevice::init("ESP32_BLE_Test");
@@ -49,29 +70,25 @@ void setup() {
   pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
 
-  pService = pServer->createService(BLEUUID(SERVICE_UUID));
+  pService = pServer->createService(SERVICE_UUID);
 
   pCharacteristic = pService->createCharacteristic(
-                      BLEUUID(CHARACTERISTIC_UUID),
-                      BLECharacteristic::PROPERTY_READ |
-                      BLECharacteristic::PROPERTY_WRITE
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE
                     );
 
   pService->start();
 
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(BLEUUID(SERVICE_UUID));
+  pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(true);
-  pAdvertising->setMinPreferred(0x06);
+  pAdvertising->setMinPreferred(0x06); 
   pAdvertising->setMinPreferred(0x12);
   BLEDevice::startAdvertising();
   Serial.println("Characteristic defined! Now you can read it in your phone!");
 
-  // Register the GAP event handler
-  esp_ble_gap_register_callback(MyServerCallbacks::gap_event_handler);
+  esp_ble_gap_register_callback(my_gap_event_handler);
 }
 
 void loop() {
-  // The loop is now empty because reading the RSSI is handled by the GAP event handler.
-  delay(1000); // Delay to prevent running the loop too quickly.
 }
--- radiowave.sql
+++ radiowave.sql
@@ -15,5 +15,15 @@ CREATE TABLE `receiver` (
                             `ip_address` VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE `training_data` (
+                                 `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+                                 `frequency` INTEGER NOT NULL,
+                                 `materials` TEXT DEFAULT NULL,
+                                 `thickness` DECIMAL(16,2) DEFAULT NULL,
+                                 `distance` DECIMAL(10,5) DEFAULT NULL,
+                                 `rssi` INTEGER NOT NULL,
+                                 `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
 ALTER TABLE `receiver`
     ADD CONSTRAINT `receiver_ibfk_1` FOREIGN KEY (`sender_id`) REFERENCES `sender` (`id`);
